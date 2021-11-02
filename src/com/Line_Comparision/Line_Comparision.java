1099a4fbd7e280077a4e0e80221e5ebe71c2c4fb
Durga Nirmal <durganirmal123@gmail.com>
2021-11-02 19:24:59 +0530

    Added : Welcome to Line comparision Computation

---
+++ src/com/Line_Comparision/Line_Comparision.java
@@ -0,0 +1,10 @@
+package com.Line_Comparision;
+
+public class Line_Comparision {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparision Computation");
+
+	}
+
+}
